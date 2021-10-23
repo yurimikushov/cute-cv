e31b6721c3c2d984f8dc885cf004a44c276d37b4
Yuri Mikushov <mikushov.yu@gmail.com>
2021-10-23 16:19:03 +0300

    chore: add color for `Tailwind` config

--- projects/frontend/tailwind.config.js
+++ projects/frontend/tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      white: '#fff',
+      black: '#000',
+      gray: {
+        50: '#e5e5e5',
+        100: '#c7c7c7',
+        200: '#adadad',
+        300: '#73808d',
+      },
+    },
   },
   variants: {
     extend: {},
