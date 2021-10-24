57d41f55ea596bb7b74763b31f7b542bd1610f97
Yuri Mikushov <mikushov.yu@gmail.com>
2021-10-24 17:27:00 +0300

    refactor: use utility type to extends HTML element props

--- projects/frontend/src/components/TextInput/TextInput.props.ts
+++ projects/frontend/src/components/TextInput/TextInput.props.ts
@@ -1,9 +1,6 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
+import { HTMLProps } from 'react'
 
-// TODO: should extract to core types
-type HTMLElementPropsT<T> = DetailedHTMLProps<HTMLAttributes<T>, T>
-
-type TextInputPropsT = Omit<HTMLElementPropsT<HTMLInputElement>, 'onChange'> & {
+type TextInputPropsT = Omit<HTMLProps<HTMLInputElement>, 'size'| 'value' | 'onChange'> & {
   size?: 'sm' | 'md' | 'lg' | 'xl' | '2xl'
   value: string
   onChange: (value: string) => void
