9f4783014b6677e990734b509eb0d7238a2e4955
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-08 16:18:20 +0300

    [front] feat: use `educations` service in UI

--- projects/frontend/src/pages/Home/Educations/Educations.tsx
+++ projects/frontend/src/pages/Home/Educations/Educations.tsx
@@ -2,39 +2,38 @@ import { FC } from 'react'
 import cn from 'classnames'
 import { useTranslation } from 'react-i18next'
 import map from 'lodash/map'
-import noop from 'lodash/noop'
+import { useEducations } from 'services/cv'
 import H from 'components/H'
 import Button from 'components/Button'
 import Education from './Education'
 import EducationsPropsT from './Educations.props'
 
-const educations = [
-  {
-    degree: '',
-    university: '',
-    duration: '',
-  },
-]
-
 const Educations: FC<EducationsPropsT> = ({ className, ...props }) => {
   const { t } = useTranslation('translation', { keyPrefix: 'education' })
+  const { educations, handleAdd, handleUpdate, handleDelete } = useEducations()
 
   return (
     <div className={cn(className, 'childs-mt-4')} {...props}>
       <H tag='1'>{t('title')}</H>
-      {map(educations, ({ degree, university, duration }, i) => (
+      {map(educations, ({ id, degree, university, duration }, i) => (
         <Education
-          key={i}
+          key={id}
           degree={degree}
           university={university}
           duration={duration}
-          onDegreeChange={noop}
-          onUniversityChange={noop}
-          onDurationChange={noop}
-          onDelete={noop}
+          onDegreeChange={(degree) =>
+            handleUpdate({ id, degree, university, duration })
+          }
+          onUniversityChange={(university) =>
+            handleUpdate({ id, degree, university, duration })
+          }
+          onDurationChange={(duration) =>
+            handleUpdate({ id, degree, university, duration })
+          }
+          onDelete={() => handleDelete({ id })}
         />
       ))}
-      <Button className='block mx-auto mt-2' onClick={noop}>
+      <Button className='block mx-auto mt-2' onClick={handleAdd}>
         Add
       </Button>
     </div>
