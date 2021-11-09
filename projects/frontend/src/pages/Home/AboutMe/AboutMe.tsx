0ed0635806312091e660fc88af49a62c732c90f0
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-09 09:52:27 +0300

    [front] feat: use `aboutMe` service in UI

--- projects/frontend/src/pages/Home/AboutMe/AboutMe.tsx
+++ projects/frontend/src/pages/Home/AboutMe/AboutMe.tsx
@@ -1,17 +1,18 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useAboutMe } from 'services/cv'
 import TextArea from 'components/TextArea'
 
 const AboutMe: FC = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'aboutMe' })
-  const [aboutMe, setAboutMe] = useState('')
+  const { aboutMe, handleChange } = useAboutMe()
 
   return (
     <TextArea
       className='w-full'
       value={aboutMe}
       placeholder={t('placeholder')}
-      onChange={setAboutMe}
+      onChange={(aboutMe) => handleChange({ aboutMe })}
     />
   )
 }
