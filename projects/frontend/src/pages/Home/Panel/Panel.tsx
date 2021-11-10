824c3d2833232828691582e5b7b00b29daa1409e
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-10 10:10:18 +0300

    [front] feat: use `editable` service in UI

--- projects/frontend/src/pages/Home/Panel/Panel.tsx
+++ projects/frontend/src/pages/Home/Panel/Panel.tsx
@@ -1,16 +1,17 @@
-import { FC, useReducer } from 'react'
+import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import cn from 'classnames'
+import { useEditable } from 'services/app'
 import Button from 'components/Button'
 import PanelPropsT from './Panel.props'
 
 const Panel: FC<PanelPropsT> = ({ className }) => {
   const { t } = useTranslation('translation', { keyPrefix: 'panel' })
-  const [editable, toggleEditable] = useReducer((editable) => !editable, true)
+  const { editable, handleSet } = useEditable()
 
   return (
     <div className={cn(className, 'flex justify-end')}>
-      <Button onClick={toggleEditable}>
+      <Button onClick={() => handleSet({ editable: !editable })}>
         {editable ? t('preview') : t('edit')}
       </Button>
     </div>
