e817a1004aa1d0c7006fa98e067032192892db76
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-11 17:11:21 +0300

    [front] feat: use `editable` mode in `about me`

--- projects/frontend/src/pages/Home/AboutMe/AboutMe.tsx
+++ projects/frontend/src/pages/Home/AboutMe/AboutMe.tsx
@@ -1,15 +1,18 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useEditable } from 'services/app'
 import { useAboutMe } from 'services/cv'
 import TextArea from 'components/TextArea'
 
 const AboutMe: FC = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'aboutMe' })
+  const { editable } = useEditable()
   const { aboutMe, handleChange } = useAboutMe()
 
   return (
     <TextArea
       className='w-full'
+      disabled={!editable}
       value={aboutMe}
       placeholder={t('placeholder')}
       onChange={(aboutMe) => handleChange({ aboutMe })}
