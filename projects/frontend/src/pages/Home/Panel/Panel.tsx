3eee426bc1315e969f903ab4edd279df8f109438
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-13 15:38:32 +0300

    [front] feat: display current saving status

--- projects/frontend/src/pages/Home/Panel/Panel.tsx
+++ projects/frontend/src/pages/Home/Panel/Panel.tsx
@@ -1,16 +1,25 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import cn from 'classnames'
-import { useEditable } from 'services/app'
+import { useEditable, useSaving } from 'services/app'
 import Button from 'components/Button'
 import PanelPropsT from './Panel.props'
 
 const Panel: FC<PanelPropsT> = ({ className }) => {
-  const { t } = useTranslation('translation', { keyPrefix: 'panel' })
+  const { t, i18n } = useTranslation('translation', { keyPrefix: 'panel' })
   const { editable, handleToggle } = useEditable()
+  const { isSaved, savedAt } = useSaving()
 
   return (
-    <div className={cn(className, 'flex justify-end')}>
+    <div className={cn(className, 'flex justify-between')}>
+      <div className='text-gray-300'>
+        {isSaved
+          ? `Saved at ${savedAt?.toLocaleDateString(i18n.language, {
+              hour: '2-digit',
+              minute: '2-digit',
+            })}`
+          : 'Not saved'}
+      </div>
       <Button onClick={handleToggle}>
         {editable ? t('preview') : t('edit')}
       </Button>
