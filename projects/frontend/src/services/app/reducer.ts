15aa6c3e39198248e06a56d1f2822626bec0ee5b
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-13 15:31:38 +0300

    [front] fix

--- projects/frontend/src/services/app/reducer.ts
+++ projects/frontend/src/services/app/reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import { editableReducer } from './editable'
 import { loadingReducer } from './loading'
+import { savingReducer } from './saving'
 
 const appReducer = combineReducers({
   editable: editableReducer,
   loading: loadingReducer,
+  saving: savingReducer,
 })
 
 export default appReducer
