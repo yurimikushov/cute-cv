b1ac89373716c01001583910da7dd96b90200578
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-13 15:42:23 +0300

    [front] chore: disable checking of serializable values

--- projects/frontend/src/services/store.ts
+++ projects/frontend/src/services/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducer'
 
-const store = configureStore({ reducer: rootReducer })
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+})
 
 type RootStateT = ReturnType<typeof rootReducer>
 
