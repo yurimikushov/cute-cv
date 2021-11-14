e56161950be8666e9e465eb1fbb2f5a3dfdb1a1a
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-14 10:46:49 +0300

    [front] refactor: extract services names to enum

---
+++ projects/frontend/src/services/constants.ts
@@ -0,0 +1,6 @@
+enum ServiceNameEnum {
+  app = 'app',
+  cv = 'cv',
+}
+
+export { ServiceNameEnum }
--- projects/frontend/src/services/reducer.ts
+++ projects/frontend/src/services/reducer.ts
@@ -1,10 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit'
+import { ServiceNameEnum } from './constants'
 import { appReducer } from './app'
 import { cvReducer } from './cv'
 
 const rootReducer = combineReducers({
-  app: appReducer,
-  cv: cvReducer,
+  [ServiceNameEnum.app]: appReducer,
+  [ServiceNameEnum.cv]: cvReducer,
 })
 
 export default rootReducer
