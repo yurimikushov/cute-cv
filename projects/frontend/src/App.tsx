287a8fcc28753ad7e8cfc2f10531ed9636d1f9f6
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-15 12:05:29 +0300

    [front] style: make page component naming consistly
    [front] fix

--- projects/frontend/src/App.tsx
+++ projects/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { useLoadCV, useSaveCV, useLoading } from 'services/app'
-import HomePage from 'pages/Home'
+import HomePage from 'pages/HomePage'
 import Loader from 'components/Loader'
 
 const App: FC = () => {
