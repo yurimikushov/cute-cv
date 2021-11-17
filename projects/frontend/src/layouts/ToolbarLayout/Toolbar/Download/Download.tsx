af7d7de54e86be8414fc47c07d5c702dc934bcf0
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-17 18:16:52 +0300

    [front] feat: use `download` service in UI

--- projects/frontend/src/layouts/ToolbarLayout/Toolbar/Download/Download.tsx
+++ projects/frontend/src/layouts/ToolbarLayout/Toolbar/Download/Download.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useDownload } from 'services/app'
 import H from 'components/H'
 import Button from 'components/Button'
 import DownloadPropsT from './Download.props'
 
 const Download: FC<DownloadPropsT> = ({ className }) => {
   const { t } = useTranslation('translation', { keyPrefix: 'toolbar' })
+  const { handleDownloadPDF } = useDownload()
 
   return (
     <div className={className}>
       <H tag='2'>{t('download.title')}</H>
-      <Button withPaddings={false}>{t('download.pdf')}</Button>
+      <Button withPaddings={false} onClick={handleDownloadPDF}>
+        {t('download.pdf')}
+      </Button>
     </div>
   )
 }
