0635da514322f9cb40efee35356af6e7f45608f8
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-18 14:06:36 +0300

    [front] feat: add sign in/out btns to panel

--- projects/frontend/src/pages/HomePage/Panel/Panel.tsx
+++ projects/frontend/src/pages/HomePage/Panel/Panel.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import cn from 'classnames'
 import { useEditable } from 'services/app'
+import { useSignInGoogle, useSignOut } from 'services/auth'
 import { useSavedSince } from './hooks'
 import Button from 'components/Button'
 import PanelPropsT from './Panel.props'
@@ -11,9 +12,14 @@ const Panel: FC<PanelPropsT> = ({ className }) => {
   const { editable, handleToggle } = useEditable()
   const savedSince = useSavedSince()
 
+  const { handleSignInGoogle } = useSignInGoogle()
+  const { handleSignOut } = useSignOut()
+
   return (
     <div className={cn(className, 'flex justify-between')}>
       <div className='text-gray-300'>{savedSince}</div>
+      <Button onClick={handleSignInGoogle}>Sign Google in</Button>
+      <Button onClick={handleSignOut}>Sign out</Button>
       <Button onClick={handleToggle}>
         {editable ? t('preview') : t('edit')}
       </Button>
