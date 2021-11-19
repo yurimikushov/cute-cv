d5b2642bc324d6182332991c47cee02c6f9d12c6
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-19 19:36:27 +0300

    [front] feat: hide sign in modal when user is signed in

--- projects/frontend/src/layouts/SignInLayout/SignInLayout.tsx
+++ projects/frontend/src/layouts/SignInLayout/SignInLayout.tsx
@@ -2,11 +2,18 @@ import { FC } from 'react'
 import SignInModal from './SignInModal'
 import SignInLayoutPropsT from './SignInLayout.props'
 
-const SignInLayout: FC<SignInLayoutPropsT> = ({ children }) => (
-  <>
-    {children}
-    <SignInModal />
-  </>
-)
+import isNull from 'lodash/isNull'
+import { useUser } from 'services/user'
+
+const SignInLayout: FC<SignInLayoutPropsT> = ({ children }) => {
+  const user = useUser()
+
+  return (
+    <>
+      {children}
+      {isNull(user) && <SignInModal />}
+    </>
+  )
+}
 
 export default SignInLayout
