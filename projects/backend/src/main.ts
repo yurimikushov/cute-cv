39b765792d3fd333f155abb4e5c5767553fe312a
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-20 18:52:36 +0300

    [back] feat: enable cors for frontend

--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 const main = async () => {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({ origin: process.env.FRONTEND_ORIGIN })
   await app.listen(process.env.PORT)
 }
 
