4667e2a72339fd5017f6dbed4ef848aaa977178b
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-22 17:42:08 +0300

    [back] sec: add `helmet` middleware

--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
+import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 
 const main = async () => {
   const app = await NestFactory.create(AppModule)
   app.enableCors({ origin: process.env.FRONTEND_ORIGIN })
+  app.use(helmet())
   await app.listen(process.env.PORT)
 }
 
