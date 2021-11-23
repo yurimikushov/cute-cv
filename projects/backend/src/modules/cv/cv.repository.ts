317224b3e97163e6da2093371eadf7c442838eac
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-23 17:56:49 +0300

    [back] refactor: use `firebase-admin` pkg for authentificated requests

--- projects/backend/src/modules/cv/cv.repository.ts
+++ projects/backend/src/modules/cv/cv.repository.ts
@@ -1,52 +1,67 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { FirebaseApp, initializeApp } from 'firebase/app'
-import {
-  FirebaseStorage,
-  getStorage,
-  ref,
-  getDownloadURL,
-  uploadBytes,
-} from 'firebase/storage'
-import axios from 'axios'
+import firebase from 'firebase-admin'
+import { getStorage, Storage } from 'firebase-admin/storage'
+import { Stream } from 'stream'
+import { getFirebaseApp } from 'lib/firebase'
 import { CV } from './cv.interface'
 
 @Injectable()
 export class CVRepository {
-  private firebaseApp: FirebaseApp
-  private storage: FirebaseStorage
+  private firebaseApp: firebase.app.App
+  private storage: Storage
 
   constructor(private configService: ConfigService) {
-    this.firebaseApp = initializeApp({
-      apiKey: this.configService.get('FIREBASE_API_KEY'),
-      projectId: this.configService.get('FIREBASE_PROJECT_ID'),
-      appId: this.configService.get('FIREBASE_APP_ID'),
-      authDomain: this.configService.get('FIREBASE_AUTH_DOMAIN'),
-      storageBucket: this.configService.get('FIREBASE_STORAGE_BUCKET'),
-      messagingSenderId: this.configService.get('FIREBASE_MESSAGING_SENDER_ID'),
-    })
+    this.firebaseApp = getFirebaseApp()
     this.storage = getStorage(this.firebaseApp)
   }
 
   async readCV(uid: string): Promise<CV | null> {
-    try {
-      const cvRef = ref(this.storage, `cv/${uid}.json`)
-      const url = await getDownloadURL(cvRef)
-      const { data: cv } = await axios.get<CV>(url)
-      return cv
-    } catch (error) {
-      console.log({ error })
+    const [isCvExists] = await this.storage
+      .bucket(this.configService.get('FIREBASE_STORAGE_BUCKET'))
+      .file(`cv/${uid}.json`)
+      .exists()
+
+    if (!isCvExists) {
       return null
     }
+
+    return new Promise((resolve, reject) => {
+      const receiver = new Stream.PassThrough()
+
+      this.storage
+        .bucket(this.configService.get('FIREBASE_STORAGE_BUCKET'))
+        .file(`cv/${uid}.json`)
+        .createReadStream()
+        .pipe(receiver)
+        .on('finish', () => {
+          resolve(receiver.read().toString())
+        })
+        .on('error', reject)
+    })
   }
 
   async updateCV(uid: string, cv: CV) {
-    const { metadata } = await uploadBytes(
-      ref(this.storage, `cv/${uid}.json`),
-      Buffer.from(JSON.stringify(cv))
-    )
+    await new Promise((resolve, reject) => {
+      const sender = new Stream.PassThrough()
+      sender.write(JSON.stringify(cv))
+      sender.end()
 
-    const { updated: savedAt } = metadata
+      sender
+        .pipe(
+          this.storage
+            .bucket(this.configService.get('FIREBASE_STORAGE_BUCKET'))
+            .file(`cv/${uid}.json`)
+            .createWriteStream()
+        )
+        .on('finish', resolve)
+        .on('error', reject)
+    })
+
+    const [{ updated: savedAt }] = await this.storage
+      .bucket(this.configService.get('FIREBASE_STORAGE_BUCKET'))
+      .file(`cv/${uid}.json`)
+      .getMetadata()
 
     return {
       savedAt,
