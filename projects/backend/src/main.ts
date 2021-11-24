69fd253b88c424e155b5f9b10cc440a7e1d250ab
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-24 20:08:16 +0300

    [back] feat: validate received cv

--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 
@@ -6,6 +7,7 @@ const main = async () => {
   const app = await NestFactory.create(AppModule)
   app.enableCors({ origin: process.env.FRONTEND_ORIGIN })
   app.use(helmet())
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT)
 }
 
--- projects/backend/src/modules/cv/dto/update.dto.ts
+++ projects/backend/src/modules/cv/dto/update.dto.ts
@@ -1,38 +1,90 @@
+import { IsString, IsArray, ValidateNested } from 'class-validator'
+import { Type } from 'class-transformer'
+
 class UpdateCvDto {
+  @IsString()
   readonly fullName: string
+
+  @IsString()
   readonly position: string
+
+  @IsString()
   readonly avatar: string
+
+  @IsString()
   readonly aboutMe: string
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => ExperienceDto)
   readonly experiences: Array<ExperienceDto>
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => EducationDto)
   readonly educations: Array<EducationDto>
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => ContactDto)
   readonly contacts: Array<ContactDto>
+
+  @IsString()
   readonly technologies: string
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => LanguageDto)
   readonly languages: Array<LanguageDto>
 }
 
 class ExperienceDto {
+  @IsString()
   readonly id: string
+
+  @IsString()
   readonly position: string
+
+  @IsString()
   readonly company: string
+
+  @IsString()
   readonly duration: string
+
+  @IsString()
   readonly description: string
 }
 
 class EducationDto {
+  @IsString()
   readonly id: string
+
+  @IsString()
   readonly degree: string
+
+  @IsString()
   readonly university: string
+
+  @IsString()
   readonly duration: string
 }
 
 class ContactDto {
+  @IsString()
   readonly id: string
+
+  @IsString()
   readonly text: string
+
+  @IsString()
   readonly href: string
 }
 
 class LanguageDto {
+  @IsString()
   readonly id: string
+
+  @IsString()
   readonly language: string
 }
 
