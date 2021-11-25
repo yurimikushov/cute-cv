b6ee9ba30dfed77127aa04337a23911391ee1bdd
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-25 18:13:43 +0300

    [front] refactor: move `cv layout` styles to css file It's needed to css applying at pdf file

--- projects/frontend/src/layouts/CVLayout/CVLayout.tsx
+++ projects/frontend/src/layouts/CVLayout/CVLayout.tsx
@@ -2,12 +2,10 @@ import { FC } from 'react'
 import cn from 'classnames'
 import { CV_CONTAINER_ID } from 'services/cv'
 import CVLayoutPropsT from './CVLayout.props'
+import './CVLayout.css'
 
 const CVLayout: FC<CVLayoutPropsT> = ({ className, children }) => (
-  <div
-    id={CV_CONTAINER_ID}
-    className={cn(className, 'grid grid-cols-cv grid-rows-cv gap-10')}
-  >
+  <div id={CV_CONTAINER_ID} className={cn(className, 'cv-layout')}>
     {children}
   </div>
 )
