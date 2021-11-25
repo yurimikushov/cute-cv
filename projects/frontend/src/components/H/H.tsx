5a13043ce853793d7d762788688dc9de1a98e932
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-25 18:57:12 +0300

    [front] refactor: move `h` styles to css file It's needed to css applying at pdf file

--- projects/frontend/src/components/H/H.tsx
+++ projects/frontend/src/components/H/H.tsx
@@ -1,30 +1,19 @@
 import { FC } from 'react'
 import cn from 'classnames'
 import HPropsT from './H.props'
+import './H.css'
 
 const H: FC<HPropsT> = ({ className, tag, children, ...props }) => {
   switch (tag) {
     case '1':
       return (
-        <h1
-          className={cn(
-            className,
-            'text-xs uppercase text-gray-300 tracking-wide'
-          )}
-          {...props}
-        >
+        <h1 className={cn(className, 'h h--1')} {...props}>
           {children}
         </h1>
       )
     case '2':
       return (
-        <h2
-          className={cn(
-            className,
-            'text-sm font-bold text-gray-300 tracking-wide'
-          )}
-          {...props}
-        >
+        <h2 className={cn(className, 'h h--2')} {...props}>
           {children}
         </h2>
       )
