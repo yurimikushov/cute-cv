8a7a48d779721760acd2328febc30a666fd608f6
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-26 18:26:59 +0300

    [front] refactor: rewrite `card` to styled-components

--- projects/frontend/src/components/Card/Card.tsx
+++ projects/frontend/src/components/Card/Card.tsx
@@ -1,25 +1,42 @@
 import { FC } from 'react'
-import cn from 'classnames'
+import styled from 'styled-components'
 import noop from 'lodash/noop'
 import { CloseButton } from 'components/Button'
 import CardPropsT from './Card.props'
-import './Card.css'
+
+const Wrapper = styled.div<CardPropsT>`
+  position: relative;
+  background-color: #fff;
+  border-radius: 5px;
+  ${({ withBorder }) =>
+    withBorder &&
+    `
+  padding: 0.5rem;
+  border: 1px solid #adadad;
+  &:hover {
+    box-shadow: 0 0 4px 0 #c7c7c7;
+  }
+  `}
+`
+
+const Close = styled(CloseButton)`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.375rem;
+`
 
 const Card: FC<CardPropsT> = ({
-  className,
   withBorder = true,
   hasClose = false,
   onClose = noop,
   children,
   ...props
 }) => (
-  <div
-    className={cn(className, 'card', { 'card--with-border': withBorder })}
-    {...props}
-  >
+  // @ts-expect-error bad typing
+  <Wrapper withBorder={withBorder} {...props}>
     {children}
-    {hasClose && <CloseButton className='card__close' onClick={onClose} />}
-  </div>
+    {hasClose && <Close onClick={onClose} />}
+  </Wrapper>
 )
 
 export default Card
