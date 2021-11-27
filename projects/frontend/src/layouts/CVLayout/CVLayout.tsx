ede7576b31e579e059f6f2161e95949d9eb9af41
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-27 12:05:47 +0300

    [front] refactor: rewrite `cv layout` to styled-components

--- projects/frontend/src/layouts/CVLayout/CVLayout.tsx
+++ projects/frontend/src/layouts/CVLayout/CVLayout.tsx
@@ -1,13 +1,20 @@
 import { FC } from 'react'
-import cn from 'classnames'
+import styled from 'styled-components'
 import { CV_CONTAINER_ID } from 'services/cv'
 import CVLayoutPropsT from './CVLayout.props'
-import './CVLayout.css'
 
-const CVLayout: FC<CVLayoutPropsT> = ({ className, children }) => (
-  <div id={CV_CONTAINER_ID} className={cn(className, 'cv-layout')}>
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: minmax(auto, 485px) 150px;
+  grid-template-rows: auto 1fr;
+  gap: 2.5rem;
+`
+
+const CVLayout: FC<CVLayoutPropsT> = ({ children, ...props }) => (
+  // @ts-expect-error bad typing
+  <Wrapper {...props} id={CV_CONTAINER_ID}>
     {children}
-  </div>
+  </Wrapper>
 )
 
 export default CVLayout
