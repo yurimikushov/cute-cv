9fc89182aebc251d5bb0224df0759841f69e37b2
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-27 11:10:42 +0300

    [front] refactor: rewrite `modal` to styled-components

--- projects/frontend/src/components/Modal/Modal.tsx
+++ projects/frontend/src/components/Modal/Modal.tsx
@@ -1,20 +1,30 @@
 import { FC } from 'react'
-import cn from 'classnames'
+import styled from 'styled-components'
 import Portal from 'components/Portal'
 import ModalPropsT from './Modal.props'
 
-const Modal: FC<ModalPropsT> = ({ className, children, ...props }) => (
-  <Portal
-    className={cn(
-      'fixed top-0 right-0 bottom-0 left-0',
-      'flex justify-center items-center',
-      'bg-overlay'
-    )}
-  >
-    <div className={cn(className, 'bg-white shadow-xs')} {...props}>
-      {children}
-    </div>
-  </Portal>
+const PortalWrapper = styled(Portal)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #e5e5e5e6;
+`
+
+const Content = styled.div`
+  background-color: #fff;
+  box-shadow: 0 0 2px 0 #e5e5e5;
+`
+
+const Modal: FC<ModalPropsT> = ({ children, ...props }) => (
+  <PortalWrapper>
+    {/* @ts-expect-error bad typing */}
+    <Content {...props}>{children}</Content>
+  </PortalWrapper>
 )
 
 export default Modal
