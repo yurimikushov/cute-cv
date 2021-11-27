69aea875852561d9afd13fe9c249dd3824a41231
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-27 21:22:03 +0300

    [front] refactor: move global styles to index.tsx

--- projects/frontend/src/index.tsx
+++ projects/frontend/src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { createGlobalStyle } from 'styled-components'
 import 'api/config'
 import store from 'services/store'
 import 'translation/config'
 import App from './App'
-import './index.css'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 13px;
+  }
+
+  body {
+    background-color: #e5e5e5;
+  }
+`
 
 render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
