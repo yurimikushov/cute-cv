f9880fdfeee2ad3eaf55d3eb626e8b12dbedad63
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-28 21:04:47 +0300

    [front] refactor: delete duplicated style

--- projects/frontend/src/components/Href/Href.ts
+++ projects/frontend/src/components/Href/Href.ts
@@ -1,16 +1,10 @@
 import styled from 'styled-components'
-import colors from 'styles/colors'
 
 const Href = styled.a.attrs({
   rel: 'noreferrer',
   target: '_blank',
 })`
   display: block;
-  color: ${colors.gray300};
-
-  &:hover {
-    color: ${colors.black};
-  }
 `
 
 export default Href
