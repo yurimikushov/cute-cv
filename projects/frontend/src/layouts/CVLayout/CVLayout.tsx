5c137d797508ced0227466571256fa8d4c08da11
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-28 11:41:18 +0300

    [front] refactor: combine basic and styled components

--- projects/frontend/src/layouts/CVLayout/CVLayout.tsx
+++ projects/frontend/src/layouts/CVLayout/CVLayout.tsx
@@ -3,18 +3,13 @@ import styled from 'styled-components'
 import { CV_CONTAINER_ID } from 'services/cv'
 import CVLayoutPropsT from './CVLayout.props'
 
-const Wrapper = styled.div`
+const CVLayout: FC<CVLayoutPropsT> = styled.div.attrs({
+  id: CV_CONTAINER_ID,
+})`
   display: grid;
   grid-template-columns: minmax(auto, 485px) 150px;
   grid-template-rows: auto 1fr;
   gap: 2.5rem;
 `
-
-const CVLayout: FC<CVLayoutPropsT> = ({ children, ...props }) => (
-  // @ts-expect-error bad typing
-  <Wrapper {...props} id={CV_CONTAINER_ID}>
-    {children}
-  </Wrapper>
-)
 
 export default CVLayout
