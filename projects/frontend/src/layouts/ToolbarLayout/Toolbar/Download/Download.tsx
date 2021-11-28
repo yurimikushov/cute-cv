aecce4ac08dbe728cede8c05bed4942b028e0cbe
Yuri Mikushov <mikushov.yu@gmail.com>
2021-11-28 11:23:53 +0300

    [front] fix: add space between items

--- projects/frontend/src/layouts/ToolbarLayout/Toolbar/Download/Download.tsx
+++ projects/frontend/src/layouts/ToolbarLayout/Toolbar/Download/Download.tsx
@@ -1,21 +1,28 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 import { useDownload } from 'services/cv'
 import { H2 } from 'components/H'
 import Button from 'components/Button'
 import DownloadPropsT from './Download.props'
+
+const Container = styled.div`
+  & > * + * {
+    margin-top: 0.25rem;
+  }
+`
 
 const Download: FC<DownloadPropsT> = (props) => {
   const { t } = useTranslation('translation', { keyPrefix: 'toolbar' })
   const { handleDownloadPDF } = useDownload()
 
   return (
-    <div {...props}>
+    <Container {...props}>
       <H2>{t('download.title')}</H2>
       <Button withPaddings={false} onClick={handleDownloadPDF}>
         {t('download.pdf')}
       </Button>
-    </div>
+    </Container>
   )
 }
 
--- projects/frontend/src/layouts/ToolbarLayout/Toolbar/Language/Language.tsx
+++ projects/frontend/src/layouts/ToolbarLayout/Toolbar/Language/Language.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 import map from 'lodash/map'
 import { LanguageEnum } from 'translation'
 import { H2 } from 'components/H'
 import Radio from 'components/Radio'
 import LanguagePropsT from './Language.props'
+
+const Container = styled.div`
+  & > * + * {
+    margin-top: 0.25rem;
+  }
+`
 
 const AVAILABLE_LANGUAGES = map(LanguageEnum)
 
@@ -12,14 +19,14 @@ const Language: FC<LanguagePropsT> = (props) => {
   const { t, i18n } = useTranslation('translation', { keyPrefix: 'toolbar' })
 
   return (
-    <div {...props}>
+    <Container {...props}>
       <H2>{t('language.title')}</H2>
       <Radio
         activeOption={i18n.language}
         options={AVAILABLE_LANGUAGES}
         onChange={(language) => i18n.changeLanguage(language)}
       />
-    </div>
+    </Container>
   )
 }
 
