ec03886ca0a15e46471312b1ff96078371d84d8c
Yuri Mikushov <mikushov.yu@gmail.com>
2021-12-02 20:24:28 +0300

    [back] fix: set request payload limit

--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
+import { json } from 'express'
 import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 
@@ -7,7 +8,8 @@ const main = async () => {
   const app = await NestFactory.create(AppModule)
   app.enableCors({ origin: process.env.FRONTEND_ORIGIN })
   app.use(helmet())
-  app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '10mb' }))
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT)
 }
 
