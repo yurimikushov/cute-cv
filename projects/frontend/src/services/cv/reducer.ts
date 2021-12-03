f1891b3f08dddcdf5a81163642fe422ed4a749a7
Yuri Mikushov <mikushov.yu@gmail.com>
2021-12-03 17:58:33 +0300

    [front] feat: persist `editable` setting

--- projects/frontend/src/services/cv/reducer.ts
+++ projects/frontend/src/services/cv/reducer.ts
@@ -1,12 +1,20 @@
 import { combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { ServiceNameEnum } from 'services'
 import { editableReducer } from './editable'
 import { loadReducer } from './load'
 import { saveReducer } from './save'
 import { metadataReducer } from './metadata'
 import { contentReducer } from './content'
 
+const persistConfig = {
+  key: `${ServiceNameEnum.cv}/editable`,
+  storage,
+}
+
 const cvReducer = combineReducers({
-  editable: editableReducer,
+  editable: persistReducer(persistConfig, editableReducer),
   load: loadReducer,
   save: saveReducer,
   metadata: metadataReducer,
