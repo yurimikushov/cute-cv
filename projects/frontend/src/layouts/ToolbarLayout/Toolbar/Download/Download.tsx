2c9864a8ec7d553a8a79a44dffe66364d90f510f
Yuri Mikushov <mikushov.yu@gmail.com>
2021-12-08 09:12:50 +0300

    [front] fix: toggle editable mode before pdf downloading if it needed

--- projects/frontend/src/layouts/ToolbarLayout/Toolbar/Download/Download.tsx
+++ projects/frontend/src/layouts/ToolbarLayout/Toolbar/Download/Download.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
-import { useDownload } from 'services/cv'
+import { useDownload, useEditable } from 'services/cv'
 import { H2 } from 'components/H'
 import Button from 'components/Button'
 import DownloadPropsT from './Download.props'
@@ -14,12 +14,28 @@ const Container = styled.div`
 
 const Download: FC<DownloadPropsT> = (props) => {
   const { t } = useTranslation('translation', { keyPrefix: 'toolbar' })
+  const prevEditableRef = useRef<boolean>(false)
+  const { editable, handleToggle } = useEditable()
   const { handleDownloadPDF } = useDownload()
+
+  const handleToggleEditableAndDownloadPDF = async () => {
+    prevEditableRef.current = editable
+
+    if (editable) {
+      handleToggle()
+    }
+
+    await handleDownloadPDF()
+
+    if (prevEditableRef.current) {
+      handleToggle()
+    }
+  }
 
   return (
     <Container {...props}>
       <H2>{t('download.title')}</H2>
-      <Button withPaddings={false} onClick={handleDownloadPDF}>
+      <Button withPaddings={false} onClick={handleToggleEditableAndDownloadPDF}>
         {t('download.pdf')}
       </Button>
     </Container>
