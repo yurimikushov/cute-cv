5d312a5416f0375ad01794d5b2941ef179013f37
Yuri Mikushov <mikushov.yu@gmail.com>
2021-12-19 10:03:18 +0300

    [back] feat: validate fields and arrays length

--- projects/backend/src/modules/cv/dto/update.dto.ts
+++ projects/backend/src/modules/cv/dto/update.dto.ts
@@ -1,11 +1,22 @@
-import { IsString, IsOptional, IsArray, ValidateNested } from 'class-validator'
+import {
+  IsString,
+  IsOptional,
+  IsArray,
+  ValidateNested,
+  MaxLength,
+  Length,
+  ArrayMaxSize,
+} from 'class-validator'
 import { Type } from 'class-transformer'
+import { NANO_ID_LENGTH } from 'modules/constants'
 
 class UpdateCvDto {
   @IsString()
+  @MaxLength(50)
   readonly fullName: string
 
   @IsString()
+  @MaxLength(50)
   readonly position: string
 
   @IsString()
@@ -13,27 +24,33 @@ class UpdateCvDto {
   readonly avatar: string
 
   @IsString()
+  @MaxLength(1_000)
   readonly aboutMe: string
 
   @IsArray()
+  @ArrayMaxSize(10)
   @ValidateNested()
   @Type(() => ExperienceDto)
   readonly experiences: Array<ExperienceDto>
 
   @IsArray()
+  @ArrayMaxSize(10)
   @ValidateNested()
   @Type(() => EducationDto)
   readonly educations: Array<EducationDto>
 
   @IsArray()
+  @ArrayMaxSize(10)
   @ValidateNested()
   @Type(() => ContactDto)
   readonly contacts: Array<ContactDto>
 
   @IsString()
+  @MaxLength(500)
   readonly technologies: string
 
   @IsArray()
+  @ArrayMaxSize(10)
   @ValidateNested()
   @Type(() => LanguageDto)
   readonly languages: Array<LanguageDto>
@@ -41,51 +58,65 @@ class UpdateCvDto {
 
 class ExperienceDto {
   @IsString()
+  @Length(NANO_ID_LENGTH)
   readonly id: string
 
   @IsString()
+  @MaxLength(50)
   readonly position: string
 
   @IsString()
+  @MaxLength(50)
   readonly company: string
 
   @IsString()
+  @MaxLength(25)
   readonly duration: string
 
   @IsString()
+  @MaxLength(1_000)
   readonly description: string
 }
 
 class EducationDto {
   @IsString()
+  @Length(NANO_ID_LENGTH)
   readonly id: string
 
   @IsString()
+  @MaxLength(75)
   readonly degree: string
 
   @IsString()
+  @MaxLength(75)
   readonly university: string
 
   @IsString()
+  @MaxLength(25)
   readonly duration: string
 }
 
 class ContactDto {
   @IsString()
+  @Length(NANO_ID_LENGTH)
   readonly id: string
 
   @IsString()
+  @MaxLength(50)
   readonly text: string
 
   @IsString()
+  @MaxLength(50)
   readonly href: string
 }
 
 class LanguageDto {
   @IsString()
+  @Length(NANO_ID_LENGTH)
   readonly id: string
 
   @IsString()
+  @MaxLength(25)
   readonly language: string
 }
 
