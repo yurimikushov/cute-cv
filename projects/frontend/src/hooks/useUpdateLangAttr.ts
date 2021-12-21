fdc16d8196ae798dbebbff8166665ade9207b3a3
Yuri Mikushov <mikushov.yu@gmail.com>
2021-12-21 17:45:01 +0300

    [front] refactor: delete unnecessary memo

--- projects/frontend/src/hooks/useUpdateLangAttr.ts
+++ projects/frontend/src/hooks/useUpdateLangAttr.ts
@@ -1,16 +1,14 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+
+const html = document.querySelector('html') as HTMLElement
 
 const useUpdateLangAttr = () => {
   const { i18n } = useTranslation()
 
-  const html = useMemo(() => {
-    return document.querySelector('html') as HTMLElement
-  }, [])
-
   useEffect(() => {
     html.setAttribute('lang', i18n.language)
-  }, [html, i18n.language])
+  }, [i18n.language])
 }
 
 export default useUpdateLangAttr
