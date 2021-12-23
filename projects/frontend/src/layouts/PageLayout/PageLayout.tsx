60ba71f44f94c9d059adc631ee660b6ba93d16a7
Yuri Mikushov <mikushov.yu@gmail.com>
2021-12-23 10:05:01 +0300

    [front] style: round up page layout

--- projects/frontend/src/layouts/PageLayout/PageLayout.tsx
+++ projects/frontend/src/layouts/PageLayout/PageLayout.tsx
@@ -1,15 +1,15 @@
 import { FC } from 'react'
 import styled from 'styled-components'
 import colors from 'styles/colors'
+import radiuses from 'styles/radiuses'
 import PageLayoutPropsT from './PageLayout.props'
 
 const PageLayout: FC<PageLayoutPropsT> = styled.div`
   max-width: 765px;
   min-height: 900px;
   background-color: ${colors.white};
-  border-top-width: 4px;
-  border-top-style: solid;
-  border-color: ${colors.black};
+  border-radius: ${radiuses.sm};
+  border-top: 4px solid ${colors.black};
 `
 
 export default PageLayout
