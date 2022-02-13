fefde336f42b98dba9eb75ead1049e6e86e9b5f2
Yuri Mikushov <mikushov.yu@gmail.com>
2022-02-13 11:29:57 +0300

    [front] feat: add custom hook to listen keyboard

---
+++ projects/frontend/src/hooks/useKeyDown.ts
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+
+type Options = {
+  code: 'KeyW' // should expand it as needed
+  altKey?: boolean
+  listener: (e: KeyboardEvent) => void
+}
+
+const useKeyDown = (options: Options) => {
+  const { code, altKey, listener } = options
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (altKey && !e.altKey) {
+        return
+      }
+
+      if (e.code !== code) {
+        return
+      }
+
+      listener(e)
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [code, altKey, listener])
+}
+
+export default useKeyDown
