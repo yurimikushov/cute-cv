453b93e114d2a8344f9315ce84b808d103d163c7
Yuri Mikushov <mikushov.yu@gmail.com>
2022-02-20 17:16:14 +0300

    [front] feat: memoize cv content

--- projects/frontend/src/services/cv/versions/selectors.ts
+++ projects/frontend/src/services/cv/versions/selectors.ts
@@ -5,49 +5,41 @@ import { RootStateT } from 'services/store'
 const selectCurrentCvId = (state: RootStateT) => state.cv.versions.currentId
 const selectCvVersions = (state: RootStateT) => state.cv.versions
 
-const selectCV = createSelector(
-  selectCurrentCvId,
-  selectCvVersions,
-  (cvId, cvVersions) => {
-    const { content } = cvVersions.byId[cvId] ?? {
-      content: {
-        fullName: '',
-        position: '',
-        aboutMe: '',
-        avatar: null,
-        experiences: { ids: [], byId: {} },
-        educations: { ids: [], byId: {} },
-        contacts: { ids: [], byId: {} },
-        technologies: '',
-        languages: { ids: [], byId: {} },
-      },
-    }
+const selectCurrentVersion = (state: RootStateT) => {
+  const id = selectCurrentCvId(state)
+  const versions = selectCvVersions(state)
 
-    const {
-      fullName,
-      position,
-      aboutMe,
-      avatar,
-      experiences,
-      educations,
-      contacts,
-      technologies,
-      languages,
-    } = content
+  return versions.byId[id]
+}
 
-    return {
-      fullName,
-      position,
-      aboutMe,
-      avatar,
-      experiences: map(experiences.ids, (id) => experiences.byId[id]),
-      educations: map(educations.ids, (id) => educations.byId[id]),
-      contacts: map(contacts.ids, (id) => contacts.byId[id]),
-      technologies,
-      languages: map(languages.ids, (id) => languages.byId[id]),
-    }
+const selectRawCvContent = (state: RootStateT) => {
+  const { content } = selectCurrentVersion(state)
+  return content
+}
+
+const selectCV = createSelector(selectRawCvContent, (content) => {
+  content ??= {
+    fullName: '',
+    position: '',
+    aboutMe: '',
+    avatar: null,
+    experiences: { ids: [], byId: {} },
+    educations: { ids: [], byId: {} },
+    contacts: { ids: [], byId: {} },
+    technologies: '',
+    languages: { ids: [], byId: {} },
   }
-)
+
+  const { experiences, educations, contacts, languages } = content
+
+  return {
+    ...content,
+    experiences: map(experiences.ids, (id) => experiences.byId[id]),
+    educations: map(educations.ids, (id) => educations.byId[id]),
+    contacts: map(contacts.ids, (id) => contacts.byId[id]),
+    languages: map(languages.ids, (id) => languages.byId[id]),
+  }
+})
 
 type CV = ReturnType<typeof selectCV>
 
