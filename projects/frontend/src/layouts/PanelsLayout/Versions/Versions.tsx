d91b8afea4981aacc5a5b9cd752ebeab62c8e075
Yuri Mikushov <mikushov.yu@gmail.com>
2022-02-27 09:48:40 +0300

    [front] feat: add abitiliy to delete cv version

--- projects/frontend/src/layouts/PanelsLayout/Versions/Versions.tsx
+++ projects/frontend/src/layouts/PanelsLayout/Versions/Versions.tsx
@@ -9,13 +9,16 @@ import {
   useCurrentCvMetadata,
   useSelectCv,
   useAddCv,
+  useDeleteCv,
+  useIsCvSaving,
+  useIsCvDeleting,
   MAX_CV_VERSIONS,
 } from 'services/cv'
 import Card from 'components/Card'
 import { H2 } from 'components/H'
 import Radio from 'components/Radio'
 import Divider from 'components/Divider'
-import Button from 'components/Button'
+import Button, { CloseButton } from 'components/Button'
 import { panelMixin } from '../mixins'
 import VersionsProps from './Versions.props'
 
@@ -23,30 +26,58 @@ const Container = styled(Card)`
   ${panelMixin}
 `
 
+const Item = styled(Radio.Item)`
+  display: inline-flex;
+  align-items: center;
+  gap: 0.25rem;
+`
+
+// eslint-disable-next-line max-statements
 const Versions: FC<VersionsProps> = (props) => {
   const { t } = useTranslation('translation', { keyPrefix: 'versions' })
   const cvCount = useCvCount()
   const allCv = useAllCvMetadata()
-  const { id } = useCurrentCvMetadata()
+  const { id, isNew, isSaved } = useCurrentCvMetadata()
   const selectCv = useSelectCv()
+  const addCv = useAddCv()
+  const deleteCv = useDeleteCv()
+  const { isCvSaving } = useIsCvSaving()
+  const { isCvDeleting } = useIsCvDeleting()
   const { isSignedIn } = useIsSignedIn()
 
-  const addCv = useAddCv()
+  const shouldDisable = (!isNew && !isSaved) || isCvSaving || isCvDeleting
+
+  const handleAddCv = () => {
+    addCv()
+  }
+
+  const handleDeleteCv = (id: string, isNew: boolean) => {
+    if (confirm('Are you sure you wanna delete this one?')) {
+      deleteCv(id, isNew)
+    }
+  }
 
   return (
     <Container {...props}>
       <H2>{t('title')}</H2>
-      <Radio value={id} vertical onChange={selectCv}>
-        {map(allCv, ({ id, name }) => (
-          <Radio.Item key={id} value={id}>
+      <Radio value={id} vertical disabled={shouldDisable} onChange={selectCv}>
+        {map(allCv, ({ id, name, isNew }) => (
+          <Item key={id} value={id}>
             {name}
-          </Radio.Item>
+            <CloseButton
+              disabled={shouldDisable}
+              onClick={(e) => {
+                e.stopPropagation() // to not affect onChange event of radio
+                handleDeleteCv(id, isNew)
+              }}
+            />
+          </Item>
         ))}
       </Radio>
       {isSignedIn && cvCount < MAX_CV_VERSIONS && (
         <>
           <Divider />
-          <Button withPaddings={false} onClick={addCv}>
+          <Button withPaddings={false} onClick={handleAddCv}>
             {t('add')}
           </Button>
         </>
