7ba37b64fd6aec0dd8d5bd7a091352c74eb44a6a
Yuri Mikushov <mikushov.yu@gmail.com>
2022-03-03 20:41:22 +0300

    [front] refactor: move cv api to `api` file from `index` file

---
+++ projects/frontend/src/api/cv/api.ts
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import isString from 'lodash/isString'
+import { LoadAllResult, LoadResult, SavePayload, SaveResult } from './model'
+
+class cvApi {
+  static async loadAll() {
+    const { status, data } = await axios.get<LoadAllResult>('/cv')
+
+    // eslint-disable-next-line no-magic-numbers
+    if (status !== 200) {
+      throw new Error(`Unexpected response status code: ${status}`)
+    }
+
+    return data
+  }
+
+  static async load(id: string) {
+    const { status, data } = await axios.get<LoadResult | ''>(`/cv/${id}`)
+
+    // eslint-disable-next-line no-magic-numbers
+    if (status !== 200) {
+      throw new Error(`Unexpected response status code: ${status}`)
+    }
+
+    if (isString(data)) {
+      return null
+    }
+
+    return data
+  }
+
+  static async save({ id, name, number, cv }: SavePayload) {
+    const { data } = await axios.put<SaveResult>(`/cv/${id}`, {
+      metadata: {
+        name,
+        number,
+      },
+      content: cv,
+    })
+
+    return data
+  }
+
+  static async delete(id: string) {
+    const { status } = await axios.delete(`/cv/${id}`)
+
+    // eslint-disable-next-line no-magic-numbers
+    if (status !== 200) {
+      throw new Error(`Unexpected response status code: ${status}`)
+    }
+  }
+}
+
+export default cvApi
