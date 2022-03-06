66d7ab5255c481fc96131177e07c6b61d3051e51
Yuri Mikushov <mikushov.yu@gmail.com>
2022-03-06 18:39:16 +0300

    [front] feat: make popup siblings inert when popup is opened

--- projects/frontend/src/components/Popup/ContentPortal/ContentPortal.tsx
+++ projects/frontend/src/components/Popup/ContentPortal/ContentPortal.tsx
@@ -1,5 +1,6 @@
 import { ForwardRefRenderFunction, forwardRef } from 'react'
 import styled from 'styled-components'
+import useInertSiblings from 'hooks/useInertSiblings'
 import Portal from 'components/Portal'
 import zIndex from 'styles/zIndex'
 import ContentPortalProps from './ContentPortal.props'
@@ -15,8 +16,10 @@ const ContentPortal: ForwardRefRenderFunction<
   HTMLDivElement,
   ContentPortalProps
 > = ({ children, ...props }, innerRef) => {
+  const { elementRef } = useInertSiblings<HTMLDivElement>()
+
   return (
-    <Portal>
+    <Portal ref={elementRef}>
       <Content {...props} ref={innerRef}>
         {children}
       </Content>
