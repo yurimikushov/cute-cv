4f5d7660842038a3e97f835508f10ffcb69a105b
Yuri Mikushov <mikushov.yu@gmail.com>
2022-03-15 12:40:16 +0300

    [front] fix: set constant width for page layout

--- projects/frontend/src/layouts/PageLayout/PageLayout.tsx
+++ projects/frontend/src/layouts/PageLayout/PageLayout.tsx
@@ -5,7 +5,7 @@ import radiuses from 'styles/radiuses'
 import PageLayoutProps from './PageLayout.props'
 
 const PageLayout: FC<PageLayoutProps> = styled.div`
-  max-width: 765px;
+  width: 765px;
   min-height: 900px;
   background-color: ${colors.white};
   border-radius: ${radiuses.sm};
