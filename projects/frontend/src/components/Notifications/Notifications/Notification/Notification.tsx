500a2f5bca082896720c63f3757324fff0e699bc
Yuri Mikushov <mikushov.yu@gmail.com>
2022-03-16 16:02:53 +0300

    [front] feat: add animation of shaking notifications

--- projects/frontend/src/components/Notifications/Notifications/Notification/Notification.tsx
+++ projects/frontend/src/components/Notifications/Notifications/Notification/Notification.tsx
@@ -4,6 +4,7 @@ import { CloseButton } from 'components/Button'
 import colors from 'styles/colors'
 import shadows from 'styles/shadows'
 import radiuses from 'styles/radiuses'
+import keyframes from 'styles/keyframes'
 import NotificationProps from './Notification.props'
 
 const Container = styled.div`
@@ -17,6 +18,12 @@ const Container = styled.div`
   background-color: ${colors.white};
   border-radius: ${radiuses.md};
   box-shadow: ${shadows.sm};
+
+  animation: ${keyframes['shake-with-delay']} 7s ease 1s infinite;
+
+  &:hover {
+    animation: none;
+  }
 `
 
 const Content = styled.div`
--- projects/frontend/src/styles/keyframes.ts
+++ projects/frontend/src/styles/keyframes.ts
@@ -9,4 +9,14 @@ export default {
       transform: rotate(360deg);
     }
   `,
+  'shake-with-delay': keyframes`
+     0% { transform:translate(0,0) }
+     1.78571% { transform:translate(3px, 0) }
+     3.57143% { transform:translate(0, 0) }
+     5.35714% { transform:translate(3px, 0) }
+     7.14286% { transform:translate(0, 0) }
+     8.92857% { transform:translate(3px, 0) }
+     10.71429% { transform:translate(0, 0) }
+     100% { transform:translate(0, 0) }
+  `,
 } as const
