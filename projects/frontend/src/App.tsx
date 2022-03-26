8c4d3ae1dc6c17d01a1583c98506beb7a4fc7267
Yuri Mikushov <mikushov.yu@gmail.com>
2022-03-26 09:46:30 +0300

    [front] feat: add route for `<SharePage />`

--- projects/frontend/src/App.tsx
+++ projects/frontend/src/App.tsx
@@ -1,8 +1,19 @@
 import { FC } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from 'pages/HomePage'
+import SharePage from 'pages/SharePage'
 
+// TODO: should lazy load pages
 const App: FC = () => {
-  return <HomePage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/cv/:id' element={<SharePage />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
