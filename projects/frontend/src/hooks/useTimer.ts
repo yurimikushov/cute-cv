7efe43f51aa5793fe8fcf2a0ec07f52d7efb9647
Yuri Mikushov <mikushov.yu@gmail.com>
2022-04-07 20:55:04 +0300

    [front] feat: add `useTimer` utility hook

---
+++ projects/frontend/src/hooks/useTimer.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react'
+import nonNullable from 'lib/nonNullable'
+
+const useTimer = (duration: number, callback: () => void) => {
+  const timerRef = useRef<NodeJS.Timeout | null>(null)
+
+  useEffect(() => {
+    // eslint-disable-next-line no-magic-numbers
+    if (duration < 0) {
+      return
+    }
+
+    timerRef.current = setTimeout(callback, duration)
+
+    return () => {
+      clearTimeout(nonNullable(timerRef.current))
+    }
+  }, [duration, callback])
+}
+
+export default useTimer
