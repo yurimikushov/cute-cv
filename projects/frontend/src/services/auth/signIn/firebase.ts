9cb7ed3a3ebc1140a06b52859b4c6c46498bbd4a
Yuri Mikushov <mikushov.yu@gmail.com>
2022-04-16 18:23:48 +0300

    [front] feat: always ask google account

--- projects/frontend/src/services/auth/signIn/firebase.ts
+++ projects/frontend/src/services/auth/signIn/firebase.ts
@@ -35,7 +35,13 @@ const signIn = async (
 }
 
 const signInGoogle = async () => {
-  await signIn(new GoogleAuthProvider())
+  const provider = new GoogleAuthProvider()
+
+  provider.setCustomParameters({
+    prompt: 'select_account',
+  })
+
+  await signIn(provider)
 }
 
 const signInFacebook = async () => {
