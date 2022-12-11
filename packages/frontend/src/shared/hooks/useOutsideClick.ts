dcc4b0281b2170ddfaf874e1e58eeb7967fa632f
Yuri Mikushov <mikushov.yu@gmail.com>
2022-12-11 20:01:08 +0400

    [front] refactor: fire outside click handler on capturing

--- packages/frontend/src/shared/hooks/useOutsideClick.ts
+++ packages/frontend/src/shared/hooks/useOutsideClick.ts
@@ -2,11 +2,15 @@ import { RefObject } from 'react'
 import useWindowEventListener from './useWindowEventListener'
 
 const useOutsideClick = (ref: RefObject<HTMLElement>, onClick: () => void) => {
-  useWindowEventListener('click', (e: MouseEvent) => {
-    if (ref.current && !ref.current.contains(e.target as HTMLElement)) {
-      onClick()
-    }
-  })
+  useWindowEventListener(
+    'click',
+    (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as HTMLElement)) {
+        onClick()
+      }
+    },
+    true
+  )
 }
 
 export default useOutsideClick
