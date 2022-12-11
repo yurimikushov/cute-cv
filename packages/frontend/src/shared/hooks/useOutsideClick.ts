c7d1c3e52e602ccdb8bb7e67c8bf74acccad741d
Yuri Mikushov <mikushov.yu@gmail.com>
2022-12-11 19:11:19 +0400

    [front] refactor: use `useEventListener` hook

--- packages/frontend/src/shared/hooks/useOutsideClick.ts
+++ packages/frontend/src/shared/hooks/useOutsideClick.ts
@@ -1,24 +1,12 @@
-import { RefObject, useEffect } from 'react'
-import isNull from 'lodash/isNull'
+import { RefObject } from 'react'
+import useEventListener from './useEventListener'
 
 const useOutsideClick = (ref: RefObject<HTMLElement>, onClick: () => void) => {
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if (isNull(ref.current)) {
-        return
-      }
-
-      if (!ref.current.contains(e.target as HTMLElement)) {
-        onClick()
-      }
+  useEventListener('click', (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target as HTMLElement)) {
+      onClick()
     }
-
-    document.addEventListener('click', handleClick)
-
-    return () => {
-      document.removeEventListener('click', handleClick)
-    }
-  }, [onClick])
+  })
 }
 
 export default useOutsideClick
