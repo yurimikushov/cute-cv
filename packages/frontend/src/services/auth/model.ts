f65f256cd08e9a7dae36eb3a11d9d222bde95004
Yuri Mikushov <mikushov.yu@gmail.com>
2022-12-31 13:13:52 +0400

    [front] refactor: add `auth` model

---
+++ packages/frontend/src/services/auth/model.ts
@@ -0,0 +1,10 @@
+import { atom } from '@reatom/framework'
+import { User as FirebaseUser } from 'shared/firebase/auth'
+
+type User = Pick<FirebaseUser, 'uid' | 'displayName' | 'email'>
+
+const isSignInCheckingAtom = atom(true, 'isSignInChecking')
+const isSignedInAtom = atom(false, 'isSignedIn')
+const userAtom = atom<User | null>(null, 'user')
+
+export { isSignInCheckingAtom, isSignedInAtom, userAtom }
